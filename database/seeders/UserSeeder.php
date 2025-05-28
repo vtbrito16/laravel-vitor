c62bc1dda3ce8f706408bbed2ba1191eaf175ab1
vitor <vtbrito16@gmail.com>
2025-05-28 10:05:47 -0300

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'vitor daniel',
+                'username' => 'vtbrito',
+                'email' => 'vtbrito2000@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('Senha1205'),
+            ],
+            [
+                'name' => 'vendedor vendor',
+                'username' => 'vendor',
+                'email' => 'vendedorvtbritor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('Senha1205'),
+            ],
+            [
+                'name' => 'Cliente user',
+                'username' => 'user',
+                'email' => 'clientevtbrito@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('Senha1205'),
+            ]
+        ]);
+    }
+}
