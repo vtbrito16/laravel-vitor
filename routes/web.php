9d2e63cbf92d13ed780790e93d49049049b92d57
vitor <vtbrito16@gmail.com>
2025-05-28 11:09:51 -0300

    comit1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\backend\VendorController;
+use App\Http\Controllers\backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// rota admin
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth')->name('admin.dashboard');
+
+// rota vendedor
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware('auth')->name('vendor.dashboard');
