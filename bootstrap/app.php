079e70d261b59aa60f34b5004c4d953fd4d1459e
vitor <vtbrito16@gmail.com>
2025-05-28 13:08:55 -0300

    comit1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
-
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\User;
+use App\Http\Middleware\Vendor;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminare\Suport\Facades\Auth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            'vendor' => \App\Http\Middleware\Vendor::class,
+            'user' => \App\Http\Middleware\User::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
